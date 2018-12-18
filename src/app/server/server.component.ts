020d34182e5b4f51392f5f305dd1f370e8dc0f81
lwerner27 <lwerner27@gmail.com>
2018-12-18 15:49:26 -0600

    did some basic property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
+  serverId: number = 10
+  serverStatus: boolean = true;
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,9 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
 
-  constructor() { }
+export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
