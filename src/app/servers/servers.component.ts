f6a131fa64837beaea316752e48d420339e7da2c
lwerner27 <lwerner27@gmail.com>
2018-12-20 10:02:53 -0600

    updated our app to do some 2 way bindings with the ngModel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,7 +12,7 @@ export class ServersComponent implements OnInit {
 
   serverCreationStatus: string = "No server was created."
 
-  serverName = ''
+  serverName = 'Server1'
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +27,7 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = "Server was created!"
   }
 
+  // This method is just here as an example but will probably never do this again.
   onUpdateServerName(event) {
     this.serverName = event.target.value
   }
